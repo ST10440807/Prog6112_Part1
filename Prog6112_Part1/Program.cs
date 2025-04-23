c88f17cd5277c153acd9fc2a99dab52d5279b1bc
ST10440807 <ST10440807@vcconnect.edu.za>
2025-04-23 18:19:52 +0200

    Add project files.

---
+++ Prog6112_Part1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;//Enables dictionary usage*
+using System.IO;//Handles file operations
+using System.Linq;
+using System.Media;//Enables audio playback
+using System.Text;
+using System.Threading.Tasks;
+/* Chatbot for cyber-security awareness
+    * Phishing scams,malware and social engineering,discuss safe password practises and recognizing suspicious links
+    */
+
+namespace CybersecurityAwarenessAssistant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Play audio greeting
+            PlayGreetingAudio("Cybersecuritychatbot");
+
+            //HeaderAscii Art 
+            Console.Title = "Cyber-Security Assistant Chatbot";
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(@"
+
+            
+ ██████╗██╗   ██╗██████╗ ███████╗██████╗       ███████╗███████╗ ██████╗██╗   ██╗██████╗ ██╗████████╗██╗   ██╗    ██████╗  ██████╗ ████████╗    
+██╔════╝╚██╗ ██╔╝██╔══██╗██╔════╝██╔══██╗      ██╔════╝██╔════╝██╔════╝██║   ██║██╔══██╗██║╚══██╔══╝╚██╗ ██╔╝    ██╔══██╗██╔═══██╗╚══██╔══╝    
+██║      ╚████╔╝ ██████╔╝█████╗  ██████╔╝█████╗███████╗█████╗  ██║     ██║   ██║██████╔╝██║   ██║    ╚████╔╝     ██████╔╝██║   ██║   ██║       
+██║       ╚██╔╝  ██╔══██╗██╔══╝  ██╔══██╗╚════╝╚════██║██╔══╝  ██║     ██║   ██║██╔══██╗██║   ██║     ╚██╔╝      ██╔══██╗██║   ██║   ██║       
+╚██████╗   ██║   ██████╔╝███████╗██║  ██║      ███████║███████╗╚██████╗╚██████╔╝██║  ██║██║   ██║      ██║       ██████╔╝╚██████╔╝   ██║       
+ ╚═════╝   ╚═╝   ╚═════╝ ╚══════╝╚═╝  ╚═╝      ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝   ╚═╝      ╚═╝       ╚═════╝  ╚═════╝    ╚═╝       
+                                                                                                                                               
+            
+");
+
+            //UI Setup and Text based greeting
+            //welcome ascii art 
+            Console.WriteLine(@"
+
+
+         __    __     _                          
+        / / /\ \ \___| | ___ ___  _ __ ___   ___ 
+        \ \/  \/ / _ \ |/ __/ _ \| '_ ` _ \ / _ \
+         \  /\  /  __/ | (_| (_) | | | | | |  __/
+          \/  \/ \___|_|\___\___/|_| |_| |_|\___|
+                                         
+           
+            
+        ");
