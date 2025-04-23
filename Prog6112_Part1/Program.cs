0f65b8f08ec5b5d8a30bd7bd4077b9927ac8ceba
ST10440807 <ST10440807@vcconnect.edu.za>
2025-04-23 19:06:06 +0200

    Added Response System

--- Prog6112_Part1/Program.cs
+++ Prog6112_Part1/Program.cs
@@ -80,3 +80,98 @@ namespace CybersecurityAwarenessAssistant
                 HandleUserQuery(userInput, userName);
             }
         }
+        //Greeting audio Method
+        static void PlayGreetingAudio(string filePath)
+        {
+            try
+            {
+                string fullPath = Path.Combine(Directory.GetCurrentDirectory(), "Greeting Audio", "Cybersecuritychatbot .wav");
+                //Error handling for audio greeting
+                if (File.Exists(fullPath))
+                {
+                    SoundPlayer player = new SoundPlayer(fullPath);
+                    player.PlaySync();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Error: File not found at {fullPath}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error playing audio: {ex.Message}");
+            }
+        }
+        //Response system
+        static void HandleUserQuery(string userInput, string userName)
+        {  //Q&A  setup
+            Dictionary<string, string> responses = new Dictionary<string, string>
+            { //Questions and responses based on user input
+                { "how are you", "I cant complain thanks for asking." },
+                { "what's your purpose", "I'm here to keep you safe online.What Safety tips would you like to know phishing,passwords or links ?" },
+                { "what can I ask you about","You can ask me about online safety tips.What Safety tips would you like to know phishing,passwords or links ?"},
+            };
+
+            if (responses.ContainsKey(userInput))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(responses[userInput]);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("I'm sorry, I don't understand. What Safety tips would you like to know?(phishing/passwords/links?");
+                string response = Console.ReadLine()?.ToLower().Trim();
+
+                if (response == "phishing")
+                {
+                    PhishingTips();//Call safety tips method
+                }
+                if (response == "passwords")
+                {
+                    PasswordTips();
+                }
+                if (response == "links")
+                {
+                    LinkTips();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Chatbot: Let me know if you have any other questions or type exit to close application.");
+                }
+            }
+        }
+        //Tips methods
+        static void PhishingTips()
+        {  //Define phishing 
+            Console.WriteLine(".What's Phishing?,Its a type of cyberattack where scammers trick you into giving away personal information" +
+                              " like passwords or credit card numbers by pretending to be a trusted source.");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Chatbot: Here are some essential tips to avoid phishing:");
+            Console.WriteLine("1.Never give passwords or bank details via email or any unverified links.");
+            Console.WriteLine("2.Don’t click on suspicious or unverified links,Hover over links before clicking to check where they lead.");
+            Console.WriteLine("3.Verify the sender by checking the email address carefully.");
+        }
+        static void PasswordTips()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Chatbot: Here are some helpful Password practises:");
+            Console.WriteLine("1. Use different passwords for each account.");
+            Console.WriteLine("2. Enable two-factor authentication (2FA).");
+            Console.WriteLine("3. Don’t share your passwords.");
+            Console.WriteLine("4. Avoid common passwords ,Don’t use “123456”, “password”, or your name.");
+        }
+        static void LinkTips()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Chatbot: Here are some  essential safe browsing practises and link safety tips:");
+            Console.WriteLine("1. Keep your browser updated,updates include patches that fix security holes.");
+            Console.WriteLine("2. Don’t download anything from untrusted or illegal sites.");
+            Console.WriteLine("3. Hover over links before clicking.");
+            Console.WriteLine("4. Don’t click links in unsolicited emails or texts.");
+        }
+    }
+}
